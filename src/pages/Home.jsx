5403e203f6dffbb30936750d63cbabcd80b746ba
chetan <awasthichetan0@gmail.com>
2022-08-09 13:43:22 +0530

    displayed results and small fetch refaactor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,16 @@
 import React,{ useState } from 'react'
 import MainPageLayout from '../components/MainPageLayout'
+import { apiGet } from '../misc/config';
 
 function Home() {
 
 const[input,setInput] =useState('');
+const[results,setResults] = useState(null);
 
 const onSearch = () =>{
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r => r.json()).then(result=>{
-        console.log(result);
-    })
-}
+  apiGet(`/search/shows?q=${input}`).then(result=>{
+    setResults(result);
+  })}
 
 const onInputChange = (ev) =>{
     setInput(ev.target.value);
@@ -21,10 +22,24 @@ const onKeyDown = (ev) =>{
     }
 }
 
+const renderResults = ( ()=> {
+    
+    if(results && results.length === 0){
+        return <div>No results</div>
+    }
+
+    if(results && results.length > 0){
+        return <div> {results.map( (item)=> <div key={item.show.id}>{item.show.name}</div> )} </div>
+    }
+
+    return null;
+})
+
   return (
     <MainPageLayout>
       <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} val={input}/>
       <button type='button' onClick={onSearch}>Search</button>
+      {renderResults()}
     </MainPageLayout>
   )
 }
