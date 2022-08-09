be4056c96d7f7677c87553eaa31398e7519961e0
chetan <awasthichetan0@gmail.com>
2022-08-09 12:24:20 +0530

    added react router

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom'
+
+function App() {
+  return <Switch>
+  
+
+  <Route exact path = "/">
+    This is home page
+  </Route>
+
+  <Route exact path = "/starred">
+    This is starred
+  </Route>
+
+  <Route>This is 404 Page</Route>
+
+
+  </Switch> 
+}
+
+export default App;
