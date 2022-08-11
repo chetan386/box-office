bb5941bda36a48475556cf52ced5a10372c62e16
chetan <awasthichetan0@gmail.com>
2022-08-11 12:59:21 +0530

    created show page components and populated them with data

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,72 +1,103 @@
-import React,{useEffect,useReducer} from 'react'
-import {useParams} from 'react-router-dom'
+/* eslint-disable no-underscore-dangle */
+import React, { useEffect, useReducer } from 'react';
+import { useParams } from 'react-router-dom';
+import Cast from '../components/show/Cast';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import ShowMainData from '../components/show/ShowMainData';
 import { apiGet } from '../misc/config';
 
 const reducer = (prevState, action) => {
-    switch(action.type){
-    
-      case 'FETCH_SUCCESS': {
-          return {isLoading: false,error: null,show: action.show};
-      }
-
-      case 'FETCH_FAILED': { 
-          return {...prevState,isLoading: false,error: action.error};
-      }
-    
-      default: 
-      return prevState;
+  switch (action.type) {
+    case 'FETCH_SUCCESS': {
+      return { isLoading: false, error: null, show: action.show };
     }
- }
+
+    case 'FETCH_FAILED': {
+      return { ...prevState, isLoading: false, error: action.error };
+    }
+
+    default:
+      return prevState;
+  }
+};
 
 const initialState = {
-    show: null,
-    isLoading: true,
-    error: null
-   }
+  show: null,
+  isLoading: true,
+  error: null,
+};
 
 function Show() {
+  // to get value of url
+  const { id } = useParams();
+  // to fetch data from api only when navigate page therefore we will destructure url and then use useEffect
+  // to store result we create a useState hook
 
-    // to get value of url
-    const { id } = useParams();
-    // to fetch data from api only when navigate page therefore we will destructure url and then use useEffect 
-// to store result we create a useState hook
+  const [{ show, isLoading, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
-   const [{show, isLoading, error}, dispatch] = useReducer(reducer,initialState)
+  useEffect(() => {
+    let isMounted = true;
+
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          dispatch({ type: 'FETCH_SUCCESS', show: results });
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          dispatch({ type: 'FETCH_FAILED', error: err.message });
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, [id]);
 
 
-    useEffect( () =>{
-      let isMounted = true;
+  if (isLoading) {
+    return <div>Data is being loaded</div>;
+  }
 
-      apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
-      .then(results =>{
-                if(isMounted){
-                    dispatch( {type: 'FETCH_SUCCESS', show: results } )
-                }
-              }).catch(err=>{
-                if(isMounted){
-                    dispatch( {type: 'FETCH_FAILED', error: err.message  } ) 
-                }
-              })
-
-              return () =>{
-                isMounted = false;
-              }
-
-    }, [id])
-
-   console.log('show',show)  
-
-    if(isLoading){
-        return <div>Data is being loaded</div>
-    }
-
-    if(error){
-        return <div>Error occured: {error}</div>
-    }
+  if (error) {
+    return <div>Error occured: {error}</div>;
+  }
 
   return (
-    <div>this is a show page</div>
-  )
+    <div>
+      <ShowMainData
+        image={show.image}
+        name={show.name}
+        rating={show.rating}
+        summary={show.summary}
+        tags={show.genres}
+      />
+
+      <div>
+        <h2>Details</h2>
+        <Details
+          status={show.status}
+          network={show.network}
+          premiered={show.premiered}
+        />
+      </div>
+
+      <div>
+        <h2>Seasons</h2>
+        <Seasons seasons={show._embedded.seasons} />
+      </div>
+
+      <div>
+        <h2>Cast</h2>
+        <Cast cast={show._embedded.cast} />
+      </div>
+    </div>
+  );
 }
 
-export default Show
+export default Show;
