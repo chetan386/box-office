8728421372f28b9515b7a8ccd1e0c939a82f286b
chetan <awasthichetan0@gmail.com>
2022-08-11 18:57:29 +0530

    styled the entire app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,72 +1,102 @@
-import React,{ useState } from 'react'
+import React, { useState } from 'react';
 import ActorGrid from '../components/actors/ActorGrid';
-import MainPageLayout from '../components/MainPageLayout'
+import CustomRadio from '../components/CustomRadio';
+import MainPageLayout from '../components/MainPageLayout';
 import ShowGrid from '../components/show/ShowGrid';
 import { apiGet } from '../misc/config';
 import { useLastQuery } from '../misc/custom-hooks';
+import {
+  RadioInputsWrapper,
+  SearchButtonWrapper,
+  SearchInput,
+} from './Home.styled';
 
 function Home() {
+  const [input, setInput] = useLastQuery();
+  const [results, setResults] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
+  const isShowsSearch = searchOption === 'shows';
 
-const[input,setInput] = useLastQuery();
-const[results,setResults] = useState(null);
-const [searchOption,setSearchOption] = useState('shows');
-const isShowsSearch = searchOption === "shows";
+  // useEffect allows us to hook into different component lifecycle events such as componentdid mount, componentwillmount
+  // to access componentdidmount
 
-// useEffect allows us to hook into different component lifecycle events such as componentdid mount, componentwillmount
-// to access componentdidmount
+  const onSearch = () => {
+    apiGet(`/search/${searchOption}?q=${input}`).then(result => {
+      setResults(result);
+    });
+  };
 
-const onSearch = () =>{
-  apiGet(`/search/${searchOption}?q=${input}`).then(result=>{
-    setResults(result);
-  })}
-
-const onInputChange = (ev) =>{
+  const onInputChange = ev => {
     setInput(ev.target.value);
-}
+  };
 
-const onKeyDown = (ev) =>{
-    if(ev.keyCode===13){
-        onSearch();
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
     }
-}
+  };
 
-const onRadioChange = (ev) =>{
-    setSearchOption(ev.target.value)
-}
+  const onRadioChange = ev => {
+    setSearchOption(ev.target.value);
+  };
 
-
-const renderResults = ( ()=> {
-    
-    if(results && results.length === 0){
-        return <div>No results</div>
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
     }
 
-    if(results && results.length > 0){
-        return results[0].show ? <ShowGrid  data = {results}/> : <ActorGrid data={results} />   
-      
+    if (results && results.length > 0) {
+      return results[0].show ? (
+        <ShowGrid data={results} />
+      ) : (
+        <ActorGrid data={results} />
+      );
     }
 
     return null;
-})
+  };
 
   return (
+   
     <MainPageLayout>
-      <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} val={input} placeholder="Search for something" />
-      <div>
-        <label htmlFor="shows-search">
-            Shows
-            <input id= 'shows-search' type="radio" value="shows" onChange={onRadioChange} checked={isShowsSearch}/>
-            </label>
-            
-            <label htmlFor="actors-search">
-            Actors
-            <input id= 'actors-search' type="radio" value="people" onChange={onRadioChange} checked={!isShowsSearch}/>
-            </label>
-      </div>
-      <button type='button' onClick={onSearch}>Search</button>
+      <SearchInput
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        val={input}
+        placeholder="Search for something"
+      />
+      <RadioInputsWrapper>
+        <div>
+          <CustomRadio
+            label="Shows"
+            id="shows-search"
+            value="shows"
+            onChange={onRadioChange}
+            checked={isShowsSearch}
+          />
+        </div>
+
+        <div>
+        <CustomRadio
+            label="Actors"
+            id="actors-search"
+            value="people"
+            onChange={onRadioChange}
+            checked={!isShowsSearch}
+          />
+        </div>
+      </RadioInputsWrapper>
+
+      <SearchButtonWrapper>
+        <button type="button" onClick={onSearch}>
+          Search
+        </button>
+      </SearchButtonWrapper>
       {renderResults()}
     </MainPageLayout>
-  )
+    
+  );
 }
 
-export default Home
+export default Home;
